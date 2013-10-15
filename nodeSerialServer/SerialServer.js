842cdecb7187d468b95dd130e02d94ed90ad71b6
tigoe <tom.igoe@gmail.com>
2013-10-14 22:24:09 -0400

    Updated SerialServer example with latest node, node-serialport, and npm

--- nodeSerialServer/SerialServer.js
+++ nodeSerialServer/SerialServer.js
@@ -10,7 +10,7 @@
 	based on the core examples for socket.io and serialport.js
 		
 	created 21 Aug 2012
-	modified 14 Oct 2012
+	modified 14 Oct 2013
 	by Tom Igoe
 	
 	Patches and improvements suggested by Steve Klise, Lia Martinez, and Will Jennings
@@ -19,19 +19,19 @@
 
 
 var serialport = require("serialport"),				// include the serialport library
-	SerialPort  = serialport.SerialPort,			// make a local instance of serial
-	app = require('express')(),						// start Express framework
+	SerialPort  = serialport.SerialPort,				// make a local instance of serial
+	app = require('express')(),							// start Express framework
   	server = require('http').createServer(app),		// start an HTTP server
-  	io = require('socket.io').listen(server);		// filter the server using socket.io
+  	io = require('socket.io').listen(server),			// filter the server using socket.io
+  	serialData = {},											// object to hold what goes out to the client
+  	portName = process.argv[2];							// third word of the command line should be serial port name
 
-var serialData = {};								// object to hold what goes out to the client
-
-server.listen(8080);								// listen for incoming requests on the server
+server.listen(8080);											// listen for incoming requests on the server
 
 console.log("Listening for new clients on port 8080");
 
 // open the serial port. Change the name to the name of your port, just like in Processing and Arduino:
-var myPort = new SerialPort("/dev/cu.usbmodem621", { 
+var myPort = new SerialPort(portName, { 
 	// look for return and newline at the end of each data packet:
 	parser: serialport.parsers.readline("\r\n") 
 });
