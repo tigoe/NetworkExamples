14a7db0188b50b660f54148942a335d29f4164f0
Tom Igoe <tigoe@mini215.itp.tsoa.nyu.edu>
2014-01-19 10:49:49 -0500

    Added node open example

---
+++ nodeOpenWindow/index.js
@@ -0,0 +1,7 @@
+var open = require('open');
+
+// open a browser window:
+open('https://npmjs.org/package/open');
+
+// you can specify the browser too, if you want:
+// open('https://npmjs.org/package/open', 'firefox');
