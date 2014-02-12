67ef6016cb9ede3d16580c6f9bee8345b1d6e1b5
tigoe <tom.igoe@gmail.com>
2014-02-11 23:26:53 -0500

    updated SerialServer to show bi-directional sending. Uses companion Arduino sketch

--- nodeSerialServer/SerialServer.js
+++ nodeSerialServer/SerialServer.js
@@ -10,7 +10,7 @@
 	based on the core examples for socket.io and serialport.js
 
 	created 21 Aug 2012
-	modified 14 Oct 2013
+	modified 11 Feb 2014
 	by Tom Igoe
 
 	Patches and improvements suggested by Steve Klise, Lia Martinez, and Will Jennings
@@ -20,17 +20,18 @@
 
 var serialport = require("serialport"),			// include the serialport library
 	SerialPort  = serialport.SerialPort,			// make a local instance of serial
-	app = require('express')(),						    // start Express framework
+	app = require('express')(),						// start Express framework
   	server = require('http').createServer(app),	// start an HTTP server
   	io = require('socket.io').listen(server),		// filter the server using socket.io
-  	serialData = {},										    // object to hold what goes out to the client
-  	portName = process.argv[2];						  // third word of the command line should be serial port name
+  	portName = process.argv[2];			// third word of the command line should be serial port name
 
+  io.set('log level', 1);
+console.log(io.settings.logger.level);
 server.listen(8080);										    // listen for incoming requests on the server
 
 console.log("Listening for new clients on port 8080");
 
-// open the serial port. Change the name to the name of your port, just like in Processing and Arduino:
+// open the serial port using the name entered on the command line:
 var myPort = new SerialPort(portName, {
 	// look for return and newline at the end of each data packet:
 	parser: serialport.parsers.readline("\r\n")
@@ -45,11 +46,15 @@ app.get('/', function (request, response) {
 io.sockets.on('connection', function (socket) {
 	// if there's a socket client, listen for new serial data:
 	myPort.on('data', function (data) {
-		// set the value property of scores to the serial string:
-		serialData.value = data;
 		// for debugging, you should see this in Terminal:
-		console.log(data);
+		//console.log(data);
 		// send a serial event to the web client with the data:
-		socket.emit('serialEvent', serialData);
+		socket.emit('serialEvent', data);
+	});
+
+	// if the client sends you data, act on it:
+	socket.on('data', function(data) {
+		console.log('received from client: ' +data);
+		myPort.write(data);
 	});
 });
