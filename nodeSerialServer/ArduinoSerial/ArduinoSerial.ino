0106b1440989c66ec5ee4282cc2b228bad305321
tigoe <tom.igoe@gmail.com>
2014-02-11 23:28:28 -0500

    updated companion Arduino sketch

--- nodeSerialServer/ArduinoSerial/ArduinoSerial.ino
+++ nodeSerialServer/ArduinoSerial/ArduinoSerial.ino
@@ -10,13 +10,16 @@
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
+  // set serial receive timeout to 10ms:
   Serial.setTimeout(10);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
+  // while there's serial data coming in, parse for a number:
   while (Serial.available()) {
      int ledState = Serial.parseInt();
+     // set  the built-in LED using the number (1 or 0):
     digitalWrite(13, ledState);
   }
   // read the input on analog pin 0:
