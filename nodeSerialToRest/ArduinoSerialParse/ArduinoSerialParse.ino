5b4f1c3f45f9e18ab8ba020b02ea5e3323a41cb8
tigoe <tom.igoe@gmail.com>
2014-02-12 00:24:34 -0500

    updated nodeSerialToRest

--- nodeSerialToRest/ArduinoSerialParse/ArduinoSerialParse.ino
+++ nodeSerialToRest/ArduinoSerialParse/ArduinoSerialParse.ino
@@ -1,13 +1,20 @@
 void setup() {
-  Serial.begin(9600);
-  Serial.setTimeout(10);
+   // initialize serial communication:
+  Serial.begin(9600);   
+  // set the serial receive timeout to 10ms:
+  Serial.setTimeout(10); 
 }
 
 void loop() {
+  // when there's serial data:
   while (Serial.available()) {
+    // parse incoming data for a number:
     int channel = Serial.parseInt();
+    // if the number's between 0 and 6:
     if (channel > 0 && channel < 6 ) {
+      // read the corresponding analog input:
       int sensor = analogRead(channel);
+      // print the result back to the server:
       Serial.println(sensor);
     }
   }
