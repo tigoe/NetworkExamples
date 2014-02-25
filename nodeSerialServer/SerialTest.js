298637ddbcab8bf5147e36de87c3f882be32a748
tigoe <tom.igoe@gmail.com>
2014-02-25 00:04:08 -0500

    Fixed nodeSerialServer property setting/getting

--- nodeSerialServer/SerialTest.js
+++ nodeSerialServer/SerialTest.js
@@ -24,13 +24,15 @@ var serialport = require("serialport"),	// include the serialport library
 	SerialPort  = serialport.SerialPort,	// make a local instance of it
 	portName = process.argv[2],				// get the serial port name from the command line
 	ledState = false;
+	
+	var brightness = 0;
 
 // open the serial port. The portname comes from the command line:
 var myPort = new SerialPort(portName, { 
 	baudRate: 9600,
 	// add an option in the serial port object 
 	// so that you can keep track of whether or not the serial port is open:
-	options: false,
+	isOpen: false,
 	// look for return and newline at the end of each data packet:
 	parser: serialport.parsers.readline("\r\n")
 });
@@ -38,15 +40,16 @@ var myPort = new SerialPort(portName, {
 // called when the serial port opens:
 myPort.on('open', function() {
 	console.log('port open');
+	console.log('baud rate: ' + myPort.options.baudRate);
 	// set options.open so you can track the port statue:
-	myPort.options.open = true;
+	myPort.options.isOpen = true;
 });
 
 // called when the serial port closes:
 myPort.on('close', function() {
 	console.log('port closed');
 	// set options.open so you can track the port statue:
-	myPort.options.open = false;
+	myPort.options.isOpen = false;
 });
 
 // called when there's an error with the serial port:
@@ -63,16 +66,26 @@ myPort.on('data', function (data) {
 
 function sendData() {
 	// make sure the port is open before you write to it:
-	if (myPort.options.open) {
-		// if ledState is true, output = 1. Else, output = 0:
-		var output = ledState?'1':'0';
-		// send out the serial port
-		console.log('sending ' + output);
-		myPort.write(output);
-		// toggle ledState:
-		ledState = !ledState;
+	if (myPort.options.isOpen) {
+		myPort.write('r' + brightness);
+		if (brightness < 100) {
+			brightness+= 10;
+		} else {
+			brightness = 0;
+		}
+		
+		
 	}
 }
 
 // set an intercal to toggle LEDState every 3 seconds:
-setInterval(sendData, 3000);
+setInterval(sendData, 1000);
+
+
+
+
+
+
+
+
+
