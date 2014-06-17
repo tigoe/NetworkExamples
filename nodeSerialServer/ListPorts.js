381964bfb5dcc7d965e152ccc89d683831c3ded7
tigoe <tom.igoe@gmail.com>
2014-06-17 17:09:44 -0400

    Simplified serial ListPorts.js

--- nodeSerialServer/ListPorts.js
+++ nodeSerialServer/ListPorts.js
@@ -8,9 +8,7 @@
 // list serial ports:
 serialport.list(function (err, ports) {
     ports.forEach(function(port) {
-      console.log(port.comName);
-      console.log(port.pnpId);
-      console.log(port.manufacturer);
+      console.log(port.comName + "\t" + port.manufacturer);
     });
   });
 
