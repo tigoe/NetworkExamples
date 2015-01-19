25d3b9437ce53cd3c7d1f3e9ff526c64f83709ab
tigoe <tom.igoe@gmail.com>
2015-01-18 17:02:06 -0800

    moved nodeTraceroute example

--- nodeTraceroute/index.js
+++
@@ -1,8 +0,0 @@
-var destination = process.argv[2];			 // get the site name from the command line
-
-console.log(destination);
-
-traceroute = require('traceroute');
-traceroute.trace(destination, function (err,hops) {
-  if (!err) console.log(hops);
-});
